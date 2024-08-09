a5d6ba39166546cafe7010cdbba0c81c4cbff627
unknown <ishanmhzn@gmail.com>
2024-08-09 15:52:08 +0545

    ishan

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username =' chai aur code'
+
+  return (
+    <>
+    <Chai/>
+    <h3>Ishan Maharjan {username}</h3>
+    </>
+  )
+}
+
+export default App
